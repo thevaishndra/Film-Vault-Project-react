e9ba39cc415d63e5d9ebaf98d3f15c33e9c4e674
thevaishndra <chandrav306m@gmail.com>
2024-08-28 12:52:47 +0530

    watchlist

--- film-vault/src/components/Navbar.jsx
+++ film-vault/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import Logo from '../VideoCam.png'
 import { Link } from 'react-router-dom'
   function Navbar() {
     return (
-        <div className = 'flex border space-x-8 items-center pl-3 py-3  bg-purple-700'>
-        <img className = 'w-[50px]' src={Logo}/>
+        <div className = 'flex border space-x-8 items-center pl-3 py-3  bg-lime-400 text-white'>
+        <img className = 'w-[40px] bg-white rounded-md p-1' src={Logo}/>
 
         <Link className = 'text-xl font-bold' to='/'>Movies</Link>
 
